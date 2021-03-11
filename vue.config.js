c86b90f0e2440e36274ee168bebb8d4b516694e6
olvlesya <lesyonoknou@gmail.com>
2021-03-11 09:40:17 +0100

    fix: update the site path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === "production"
-      ? "https://olvlesya.github.io/webbot/dist/"
+      ? "https://olvlesya.github.io/webbot/"
       : "/"
 };
